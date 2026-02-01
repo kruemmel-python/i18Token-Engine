fbc66f2047d7eaebf140101317bc11fe2325828d
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2026-02-01 12:35:54 +0100

    Add files via upload

---
+++ temp_search.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+text = Path('D:/i18Token/Beispiel_Programm_Python/game.py').read_text().splitlines()
+for i,line in enumerate(text,1):
+    if '000Q' in line:
+        print(i, line)
